6349d62313730fe375411bcca151c2d8552acc04
Hsiang <61619308+Arlyan-Hsiang@users.noreply.github.com>
2020-12-15 16:51:39 +1300

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,85 @@
 //For Size selection
-let buttonS = document.getElementById('S-size');
-let buttonM = document.getElementById('M-size');
-let buttonL = document.getElementById('L-size');
-let buttonOK = document.getElementById('comfirm');
+const buttonS = document.getElementById('S-size');
+const buttonM = document.getElementById('M-size');
+const buttonL = document.getElementById('L-size');
+const buttonOK = document.getElementById('comfirm');
+const cartButton = document.getElementById('button');
+const cartPreview = document.getElementById('cart-preview');
 //for variable number
 let mySize = document.querySelector('table tr td i1');
-let myAmount = document.querySelector('h1 a1');
+let myAmount = document.querySelector('a a1');
 //for calculation
-let amount = 0;
-//While click Size S
-buttonS.onclick = function() {
-        mySize.innerHTML = 'S';
-    }
-    //While click Size M
-buttonM.onclick = function() {
-        mySize.innerHTML = 'M';
+let totalAmount = 0;
+let AmountS = 0;
+let AmountM = 0;
+let AmountL = 0;
+let sizeAmount = {};
+//Array for cart
+let product = new Array();
+let proContent = '';
 
-    }
-    //While click Size L
+//size choose
+buttonS.onclick = function() {
+    mySize.innerHTML = 'S';
+}
+buttonM.onclick = function() {
+    mySize.innerHTML = 'M';
+}
 buttonL.onclick = function() {
         mySize.innerHTML = 'L';
-    }
-    //While click "Comfirm"
+}
+//confirm for adding to the cart
 buttonOK.onclick = function() {
-    if (mySize.innerHTML == 'S' ||
-        mySize.innerHTML == 'M' ||
-        mySize.innerHTML == 'L') {
-        amount++;
-        myAmount.innerHTML = amount.toString();
-    } else {
-        alert('Please choose your size.')
+        calAmount(mySize.innerHTML);
+        myAmount.innerHTML = totalAmount.toString();
+        proContent = cartPreview.querySelectorAll('p');
+        buildPreview();
+}
+//When press the cart, there is a preview.
+cartButton.onclick = function() {
+        cartPreview.classList.toggle('Active');
+}
+//function for calculating the amount of t-shirt 
+function calAmount(mySize) {
+    //if there is no correnspond size, add it in the array
+    if (product.indexOf(mySize) < 0) {
+        product.push(mySize);
+        console.log(product);
+    }
+    console.log(sizeAmount);
+    switch (mySize) {
+        case 'S':
+            totalAmount++;
+            AmountS++;
+            sizeAmount['S'] = AmountS;
+            break;
+        case 'M':
+            totalAmount++;
+            AmountM++;
+            sizeAmount['M'] = AmountM;
+            break;
+        case 'L':
+            totalAmount++;
+            AmountL++;
+            sizeAmount['L'] = AmountL;
+            break;
+        default:
+            alert('Please choose your size.');
+            break;
     }
 
+}
+//for cart preview
+function buildPreview() {
+    const plength = product.length;
+    if (plength > 0) {
+        for (let i = 0; i < plength; i++) {
+            proContent[i].innerHTML = "<table class='cart'><tr><td><img class= 'cart' src='images/tee.jpg'></td><td>" +
+                "Class Tee<br>" + sizeAmount[product[i]] + " x $100<br>" +
+                "Size " + product[i] + "</td>";
+        }
+    } 
+    else {
+        alert('nothing inside your cart.');
+    }
 }
